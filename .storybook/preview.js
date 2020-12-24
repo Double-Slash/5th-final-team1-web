176cd5159dbf23d23c2d6f1ebd1b44ef857d294b
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-12-25 02:01:07 +0900

    [변경] : 테스트 환경 변경(enzyme)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import React from "react";
+import StyleReset from "../src/utils/styles/reset";
+
+export const decorators = [
+  (Story) => (
+    <>
+      <StyleReset />
+      <Story />
+    </>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
-  setupFiles: ["react-app-polyfill/jsdom"],
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-  testMatch: ["<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}", "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"],
-  testEnvironment: "jest-environment-jsdom-fourteen",
-  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$", "^.+\\.module\\.(css|sass|scss)$"],
-  modulePaths: [],
-  moduleFileExtensions: ["web.js", "js", "web.ts", "ts", "web.tsx", "tsx", "json", "web.jsx", "jsx", "node"],
-  watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
-  moduleDirectories: ["node_modules", "src"],
+  setupFilesAfterEnv: ["<rootDir>src/setupTests.ts"],
 };
