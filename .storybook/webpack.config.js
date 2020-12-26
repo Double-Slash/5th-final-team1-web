01aaee374c4f9c7cc156854a469df7c50aa1c207
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-12-26 23:35:55 +0900

    [변경] : storybook 절대 경로 파일 추가(component, pages, utils)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,6 +4,9 @@ module.exports = ({ config }) => {
   config.resolve.alias = {
     ...config.resolve.alias,
     "@common": path.resolve(__dirname, "../src", "common"),
+    "@component": path.resolve(__dirname, "../src", "component"),
+    "@pages": path.resolve(__dirname, "../src", "pages"),
+    "@utils": path.resolve(__dirname, "../src", "utils"),
   };
 
   return config;
