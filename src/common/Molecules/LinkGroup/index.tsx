cb8593f5ae001a6db379c4b22cc1b0ba4c7bf1c1
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-12-26 19:17:55 +0900

    [추가] : LinkGroup 컴포넌트 추가

---
+++ src/common/Molecules/LinkGroup/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "@common/Atoms/Link";
+import * as S from "./style";
+
+export interface LinkGroupProps {
+  linkNamingList: string[];
+}
+
+const LinkGroup = ({ linkNamingList }: LinkGroupProps) => {
+  return (
+    <>
+      <S.Nav>
+        {linkNamingList.map((value) => (
+          <Link key={value} name={value} />
+        ))}
+      </S.Nav>
+    </>
+  );
+};
+
+export default LinkGroup;
---
+++ src/common/Molecules/LinkGroup/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  text-align: center;
+  a:not(:first-child) {
+    margin-left: 32px;
+  }
+`;
