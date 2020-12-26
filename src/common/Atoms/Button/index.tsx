c2932d95aed7ef042ab4a5ce0c0b4136f33cd90d
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-12-26 17:38:59 +0900

    [변경] : 버튼 컴포넌트 변경

--- src/common/Atoms/Button/index.tsx
+++ src/common/Atoms/Button/index.tsx
@@ -1,23 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import * as S from "./style";
 
+/**
+ * @param onClick 버튼을 클릭했을 때 실행되는 함수
+ * @param isLinked 페이지 이동을 위한 버튼인지 체크
+ * @param width 버튼 크기의 사이즈
+ * @param buttonColor 버튼의 색상
+ * @param fontColor 버튼 글자의 색상
+ * @param pathname 페이지 이동 버튼일 때 to property의 pathname
+ */
+
 export interface ButtonProps {
+  onClick: () => void;
+  isLinked: boolean;
+  width?: string;
+  buttonColor?: string;
+  fontColor?: string;
+  pathname?: string;
   children?: React.ReactNode;
-  curryingArgs?: string;
-  text?: string;
-  onClick?: () => void;
-  onClickCurrying?: (type: string) => () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, text, curryingArgs, onClick, onClickCurrying }) => {
+const Button = ({ onClick, width, buttonColor, fontColor, isLinked, pathname, children }: ButtonProps) => {
+  // 링크형 버튼일 경우
+  if (isLinked) {
+    return (
+      <S.Button onClick={onClick} width={width} buttonColor={buttonColor} fontColor={fontColor}>
+        <Link to={`/${pathname}`}>{children}</Link>
+      </S.Button>
+    );
+  }
+
+  // 링크형 버튼이 아닐 경우
   return (
     <>
-      <S.Button
-        data-testid={curryingArgs}
-        onClick={curryingArgs && onClickCurrying ? onClickCurrying(curryingArgs) : onClick}
-      >
+      <S.Button onClick={onClick} width={width} buttonColor={buttonColor} fontColor={fontColor}>
         {children}
-        {text}
       </S.Button>
     </>
   );
