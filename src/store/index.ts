3fc9c3897361649e986b24fc35b8d46e30d6f1d3
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-03 00:16:07 +0900

    [수정] : store 위치 수정

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,21 @@
-import { combineReducers } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore, Middleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import markdown from "./MarkDown";
 
 const rootReducer = combineReducers({
   markdown,
 });
 
+const store = () => {
+  const middlewares: Middleware[] = [];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
+  const config = createStore(rootReducer, enhancer);
+  return config;
+};
+
 export type rootState = ReturnType<typeof rootReducer>;
 
-export default rootReducer;
+export default store;
