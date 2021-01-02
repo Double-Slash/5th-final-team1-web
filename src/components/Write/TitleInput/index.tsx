c8adb4b56c46477ae7607f1539fd74729e7e1215
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-02 21:23:05 +0900

    [추가] : Textarea 컴포넌트 추가

--- src/components/Write/TitleInput/index.tsx
+++ src/components/Write/TitleInput/index.tsx
@@ -1,27 +1,21 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback } from "react";
 import { useDispatch } from "react-redux";
-import { changeMarkDownTitle } from "@store/Write/action";
-import * as S from "./style";
+import { changeMarkDownTitle } from "@store/MarkDown/action";
+import Textarea from "@common/Atoms/Textarea";
 
 const TitleInput = () => {
   const dispatch = useDispatch();
-  const [text, setText] = useState("");
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const changeTextarea = useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      if (!textareaRef.current) return;
-      textareaRef.current.style.height = "1px";
-      textareaRef.current.style.height = `${12 + textareaRef.current.scrollHeight}px`;
-      setText(e.target.value);
-      dispatch(changeMarkDownTitle(e.target.value));
+    (text: string) => {
+      dispatch(changeMarkDownTitle(text));
     },
     [dispatch],
   );
 
   return (
     <>
-      <S.Textarea ref={textareaRef} value={text} onChange={changeTextarea} placeholder="제목을 입력하세요" />
+      <Textarea onChange={changeTextarea} />
     </>
   );
 };
