54fb7ef1d8d57be86b47e27958aa1061451fad03
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-07 03:06:22 +0900

    [변경] : Titleinput 해시태그 로직 추가

---
+++ src/components/Write/TitleInput/style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Layout = styled.header`
+  width: 100%;
+  .tag-list {
+    display: flex;
+  }
+  button {
+    color: black;
+  }
+  textarea {
+    padding: 8px 8px 0 8px;
+    margin-bottom: 8px;
+    border-bottom: 1px solid gray;
+  }
+  input {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 8px;
+    border: none;
+    border-bottom: 1px solid gray;
+    outline: none;
+  }
+`;
