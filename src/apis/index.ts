fc0f87c891b0eadd39e66ccb6d572dfc2c472842
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-06 21:08:08 +0900

    [추가] : api 폴더 추가

---
+++ src/apis/index.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { getAccessToken } from "@utils/modules/token";
+
+const { SERVER_URL } = process.env;
+
+const defaultAxios = axios.create({
+  baseURL: `${SERVER_URL}/`,
+});
+
+const createInstanceWithToken = () => {
+  const instance = defaultAxios;
+  defaultAxios.interceptors.request.use((config) => {
+    const token = getAccessToken({ key: "access" });
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  });
+  return instance;
+};
+
+const withTokenAxios = createInstanceWithToken();
+
+export { defaultAxios, withTokenAxios };
