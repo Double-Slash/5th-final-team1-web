9e0d1cdf25f61f6d27911e33e8b334dbc6e0474f
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-07 03:08:09 +0900

    [변경] : EditorFooter 제출하기 기능 적용

--- src/components/Write/EditorFooter/index.tsx
+++ src/components/Write/EditorFooter/index.tsx
@@ -1,14 +1,32 @@
 import React, { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { rootState } from "@store/index";
 import Button from "@common/Atoms/Button";
+import { postQuestion } from "@apis/write";
 import * as S from "./style";
 
 const EditorFooter = () => {
-  const onClickSubmit = useCallback(() => {}, []);
+  const history = useHistory();
+  const title = useSelector((state: rootState) => state.markdown.title);
+  const body = useSelector((state: rootState) => state.markdown.markDownText);
+  const bounty = useSelector((state: rootState) => state.write.bounty);
+  const hashtags = useSelector((state: rootState) => state.write.tagList);
+
+  // 제출하기 버튼 클릭
+  const submitQuestion = useCallback(
+    async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const result = await postQuestion({ title, body, bounty, hashtags });
+      history.replace(`/post/${result.data.id}`);
+    },
+    [body, bounty, hashtags, history, title],
+  );
 
   return (
     <>
-      <S.Layout>
-        <Button isLinked={false} fontColor="white" buttonColor="black" onClick={onClickSubmit}>
+      <S.Layout onSubmit={submitQuestion}>
+        <Button isLinked={false} onClick={() => {}} type="submit" fontColor="white" buttonColor="black">
           제출하기
         </Button>
       </S.Layout>
