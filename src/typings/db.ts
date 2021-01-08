9f66192aeb6445e0500da033a1010c9326c597a4
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-09 00:20:21 +0900

    [변경] : db.ts 갱신

--- src/typings/db.ts
+++ src/typings/db.ts
@@ -1,26 +1,33 @@
-export interface IPost {
-  title: string; // 제목
-  hashTagList: string[]; // 해시태그 리스트
-  user: IUser; // 유저 정보
-  date: string; // 작성일자
-  content: string; // 본문
-  commentList: IComment[]; // 댓글 리스트
+export interface IQuestion {
+  id: number;
+  hashtags: string[];
+  answers: IAnswer[];
+  num_likes: string;
+  author: string;
+  title: string;
+  body: string;
+  bounty: number;
+  created_at: string;
+  edited_at: string;
+}
+
+export interface IAnswer {
+  id: number;
+  body: string;
+  created_at: string;
+  edited_at: string;
+  author: string;
+  comments: IComment[];
+  num_likes: number;
 }
 
 export interface IComment {
-  user: IUser; // 유저 정보
-  content: string; // 댓글 본문
-  childComments?: {
-    user: IUser;
-    content: string;
-  }[];
-}
-
-export interface IUser {
-  thumbnail: string; // 썸네일
-  username: string; // 유저 이름
-  rank: string; // 계급
-  postLikes: number[]; // 게시글 좋아요 누른 리스트
-  commentLikes: number[]; // 댓글 좋아요 누른 리스트
-  commentDislikes: number[]; // 댓글 싫어요 누른 리스트
+  id: number;
+  author: string;
+  num_likes: number;
+  body: string;
+  created_at: string;
+  edited_at: string;
+  question: number;
+  answer: number;
 }
