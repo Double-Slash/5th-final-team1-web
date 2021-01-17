c074b460b3773bbc4bfc663a6319c24e915ba35c
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-17 19:27:21 +0900

    [변경] : 프로젝트 상세 페이지 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,8 +5,9 @@ import Header from "@common/Organisms/Header";
 
 // page components
 const Callback = loadable(() => import("@pages/Callback"));
-const Post = loadable(() => import("@pages/Post"));
 const Questions = loadable(() => import("@pages/Questions"));
+const Post = loadable(() => import("@pages/Post"));
+const Project = loadable(() => import("@pages/Project"));
 const Write = loadable(() => import("@pages/Write"));
 const WriteProject = loadable(() => import("@pages/WriteProject"));
 
@@ -18,6 +19,7 @@ const Router = () => {
         <Switch>
           <Route path="/qna" component={Questions} exact />
           <Route path="/post/:id" component={Post} exact />
+          <Route path="/project/:id" component={Project} exact />
           <Route path="/write" component={Write} exact />
           <Route path="/write/project" component={WriteProject} exact />
           <Route path="/callback" component={Callback} exact />
