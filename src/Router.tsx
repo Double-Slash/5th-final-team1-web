48620a6f7fe29cac6e7f22e7c863ab8bee56f999
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-17 18:58:06 +0900

    [변경] : 프로젝트 모집 페이지 라우팅 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,6 +8,7 @@ const Callback = loadable(() => import("@pages/Callback"));
 const Post = loadable(() => import("@pages/Post"));
 const Questions = loadable(() => import("@pages/Questions"));
 const Write = loadable(() => import("@pages/Write"));
+const WriteProject = loadable(() => import("@pages/WriteProject"));
 
 const Router = () => {
   return (
@@ -18,6 +19,7 @@ const Router = () => {
           <Route path="/qna" component={Questions} exact />
           <Route path="/post/:id" component={Post} exact />
           <Route path="/write" component={Write} exact />
+          <Route path="/write/project" component={WriteProject} exact />
           <Route path="/callback" component={Callback} exact />
         </Switch>
       </BrowserRouter>
