28f922ea1f95bc7c548801aa8f1e0af3db144272
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-17 19:27:48 +0900

    [변경] : isBsicHashTag props 적용

--- src/common/Atoms/HashTag/style.ts
+++ src/common/Atoms/HashTag/style.ts
@@ -1,7 +1,23 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const HashTag = styled(Link)`
+export const HashTag = styled.div`
+  display: inline-flex;
+  height: 32px;
+  padding: 4px 16px;
+  border: 1px solid #a3a3a4;
+  border-radius: 1em;
+  font-weight: bold;
+  font-size: 1rem;
+  color: #266ce6;
+  cursor: pointer;
+  &::before {
+    content: "#";
+    margin-right: 2px;
+  }
+`;
+
+export const HashTagLink = styled(Link)`
   display: inline-flex;
   height: 32px;
   padding: 4px 16px;
