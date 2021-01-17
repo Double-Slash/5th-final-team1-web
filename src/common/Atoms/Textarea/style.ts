eeb349a0b6e12aed1fe9d6935f7ea054b0646c5d
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-17 19:28:51 +0900

    [수정] : Textarea default height 적용

--- src/common/Atoms/Textarea/style.ts
+++ src/common/Atoms/Textarea/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Textarea = styled.textarea`
   width: 100%;
-  padding: 8px;
+  height: 28px;
   resize: none;
   outline: none;
   border-image: initial;
