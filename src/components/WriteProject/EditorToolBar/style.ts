d7c23fc4e1688c2fc6b3fb7546842b60bc699db6
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-17 19:33:30 +0900

    [추가] : WriteProject/EditorToolBar 컴포넌트 추가

---
+++ src/components/WriteProject/EditorToolBar/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Layout = styled.article`
+  margin-bottom: 8px;
+  border-bottom: 1px solid gray;
+  button {
+    padding: 8px 16px;
+  }
+`;
