8fee87e2420b3b12f9e0c23166de4061b64a4e05
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-20 01:44:28 +0900

    [추가] : projectHeaderSVG 컴포넌트 추가

---
+++ src/static/projectHeaderSvg.tsx
@@ -0,0 +1,222 @@
+import React from "react";
+
+const ProjectHeaderSvg = () => {
+  return (
+    <>
+      <svg preserveAspectRatio="xMidYMid meet" width="452.175" height="146.538" viewBox="0 0 452.175 146.538">
+        <circle
+          id="타원_25"
+          data-name="타원 25"
+          cx="23"
+          cy="23"
+          r="23"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 3.209, 97.441)"
+          fill="#772ffe"
+          opacity="0.35"
+        />
+        <line
+          id="선_77"
+          data-name="선 77"
+          y1="45"
+          x2="88"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 30.217, 76.775)"
+          fill="none"
+          stroke="#a3a3a4"
+          strokeWidth="2"
+        />
+        <line
+          id="선_78"
+          data-name="선 78"
+          x1="1"
+          y2="54"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 121.769, 29.045)"
+          fill="none"
+          stroke="#a3a3a4"
+          strokeWidth="2"
+        />
+        <circle
+          id="타원_26"
+          data-name="타원 26"
+          cx="23"
+          cy="23"
+          r="23"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 213.89, 52.027)"
+          fill="#194593"
+          opacity="0.35"
+        />
+        <circle
+          id="타원_27"
+          data-name="타원 27"
+          cx="23"
+          cy="23"
+          r="23"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 392.733, 60.523)"
+          fill="#266ce6"
+          opacity="0.35"
+        />
+        <line
+          id="선_79"
+          data-name="선 79"
+          x1="26"
+          y1="26"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 119, 82.983)"
+          fill="none"
+          stroke="#a3a3a4"
+          strokeWidth="2"
+        />
+        <line
+          id="선_80"
+          data-name="선 80"
+          y1="43"
+          x2="87"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 147.978, 69.973)"
+          fill="none"
+          stroke="#a3a3a4"
+          strokeWidth="2"
+        />
+        <line
+          id="선_81"
+          data-name="선 81"
+          y1="41"
+          x2="77"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 364.086, 32.958)"
+          fill="none"
+          stroke="#a3a3a4"
+          strokeWidth="2"
+        />
+        <line
+          id="선_82"
+          data-name="선 82"
+          x2="53"
+          y2="7"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 361.227, 73.858)"
+          fill="none"
+          stroke="#a3a3a4"
+          strokeWidth="2"
+        />
+        <line
+          id="선_83"
+          data-name="선 83"
+          x1="82"
+          y1="56"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 283.332, 12.274)"
+          fill="none"
+          stroke="#a3a3a4"
+          strokeWidth="2"
+        />
+        <line
+          id="선_84"
+          data-name="선 84"
+          y1="11"
+          x2="125"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 236.53, 65.138)"
+          fill="none"
+          stroke="#a3a3a4"
+          strokeWidth="2"
+        />
+        <line
+          id="선_85"
+          data-name="선 85"
+          x2="69"
+          y2="44"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 235.764, 76.112)"
+          fill="none"
+          stroke="#a3a3a4"
+          strokeWidth="2"
+        />
+        <circle
+          id="타원_28"
+          data-name="타원 28"
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 14.343, 110.249)"
+          fill="#772ffe"
+        />
+        <circle
+          id="타원_29"
+          data-name="타원 29"
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 108.33, 70.709)"
+          fill="#266ce6"
+        />
+        <circle
+          id="타원_30"
+          data-name="타원 30"
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 134.309, 100.594)"
+          fill="#8eb2f0"
+        />
+        <circle
+          id="타원_31"
+          data-name="타원 31"
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 225.094, 63.837)"
+          fill="#194593"
+        />
+        <circle
+          id="타원_32"
+          data-name="타원 32"
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 430.229, 26.055)"
+          fill="#8eb2f0"
+        />
+        <circle
+          id="타원_33"
+          data-name="타원 33"
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 403.867, 73.331)"
+          fill="#266ce6"
+        />
+        <circle
+          id="타원_34"
+          data-name="타원 34"
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 272.662, 0)"
+          fill="#266ce6"
+        />
+        <circle
+          id="타원_35"
+          data-name="타원 35"
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 350.557, 61.584)"
+          fill="#772ffe"
+        />
+        <circle
+          id="타원_36"
+          data-name="타원 36"
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 290.856, 112.543)"
+          fill="#266ce6"
+        />
+        <circle
+          id="타원_37"
+          data-name="타원 37"
+          cx="11"
+          cy="11"
+          r="11"
+          transform="matrix(0.998, 0.07, -0.07, 0.998, 112.236, 14.845)"
+          fill="#194593"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default ProjectHeaderSvg;
