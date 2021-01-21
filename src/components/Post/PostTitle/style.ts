909aac6567842be55669360870eceb1577624972
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-22 02:28:43 +0900

    [변경] : PostContent, PostTitle 컴포넌트 변경

--- src/components/Post/PostTitle/style.ts
+++ src/components/Post/PostTitle/style.ts
@@ -4,23 +4,33 @@ export const PostTitle = styled.section`
   padding: 24px 0;
   border-bottom: 1px solid #bababa;
   h1 {
+    font-size: 1.6rem;
     margin: 16px 0;
+  }
+  .hash-tag-list {
+    display: flex;
+    align-items: center;
+    .login-check {
+      margin-left: auto;
+      font-size: 24px;
+    }
   }
   .user-info {
     display: flex;
     align-items: center;
     img,
-    h2 {
+    .user-name {
       margin-right: 8px;
     }
-    h2 {
-      font-size: 1.1rem;
+    .user-name {
+      font-size: 1.2rem;
+      font-weight: bold;
     }
     h3 {
       font-size: 1rem;
       color: gray;
     }
-    span {
+    .created-at {
       margin-left: auto;
       color: gray;
     }
