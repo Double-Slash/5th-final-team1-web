e44d95e0657075e42e2b774ae4620577fbacaa23
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-24 12:32:11 +0900

    [변경]: 검색 페이지 컴포넌트 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,6 +10,7 @@ const Questions = loadable(() => import("@pages/Questions"));
 const Post = loadable(() => import("@pages/Post"));
 const Project = loadable(() => import("@pages/Project"));
 const Projects = loadable(() => import("@pages/Projects"));
+const Search = loadable(() => import("@pages/Search"));
 const Write = loadable(() => import("@pages/Write"));
 const WriteProject = loadable(() => import("@pages/WriteProject"));
 
@@ -24,6 +25,7 @@ const Router = () => {
           <Route path="/post/:id" component={Post} exact />
           <Route path="/project/:id" component={Project} exact />
           <Route path="/project" component={Projects} exact />
+          <Route path="/search" component={Search} exact />
           <Route path="/write" component={Write} exact />
           <Route path="/write/project" component={WriteProject} exact />
           <Route path="/callback" component={Callback} exact />
