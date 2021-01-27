57b2ae6f478e0f00a2f49b8fc5549fdeaa70011b
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-28 01:50:24 +0900

    [추가] : 답변 PATCH api 적용

--- src/apis/answer.ts
+++ src/apis/answer.ts
@@ -1,6 +1,18 @@
 import { withTokenAxios } from "./index";
 
+interface IPatchAnswers {
+  body: string;
+  id: number;
+  is_adopted: boolean;
+  question: number;
+}
+
 // 답변 POST
 export const postAnsewrs = ({ body, question }: { body: string; question: number }) => {
   return withTokenAxios.post("/answers/viewset/", { body, question });
+};
+
+// 답변 PATCH
+export const patchAnsewrs = ({ body, id, is_adopted, question }: IPatchAnswers) => {
+  return withTokenAxios.patch(`/answers/viewset/${id}/`, { body, is_adopted, question });
 };
