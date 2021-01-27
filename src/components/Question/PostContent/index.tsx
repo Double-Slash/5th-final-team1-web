a6f5e753e578302709e4749957794df72d78a741
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-27 14:41:52 +0900

    [변경] : 질문글 수정하기 적용

--- src/components/Question/PostContent/index.tsx
+++ src/components/Question/PostContent/index.tsx
@@ -1,44 +1,80 @@
 import React, { useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "@common/Atoms/Button";
 import MarkDownInput from "@common/Organisms/MarkDownInput";
 import MarkDownRendering from "@common/Organisms/MarkDownRendering";
 import MarkDownToolBar from "@common/Organisms/MarkDownToolBar";
 import { TabList, TabMenu, TabPanel } from "@common/Organisms/TabMenu";
 import useDecodeToken from "@hooks/useDecodeToken";
+import { patchQuestion } from "@apis/question";
+import { rootState, reduxClear } from "@store/index";
 import * as S from "./style";
 
 export interface PostContentProps {
   author_id: number;
   body: string;
+  questionId: number;
+  title: string;
 }
 
-const PostContent = ({ body, author_id }: PostContentProps) => {
-  const [toggle, setToggle] = useState(true);
+const PostContent = ({ author_id, body, questionId, title }: PostContentProps) => {
+  const [toggle, setToggle] = useState(false);
+  const [renewBody, setRenewBody] = useState(body);
   const { user_id: token_id } = useDecodeToken();
+  const markDownText = useSelector<rootState>((state) => state.markdown.markDownText);
+  const disptach = useDispatch();
 
-  const clickModifyButton = useCallback(() => {
+  const clickModifyButton = useCallback(async () => {
+    try {
+      if (toggle) {
+        const { data } = await patchQuestion({ body: markDownText as string, id: questionId, title });
+        setRenewBody(data.body);
+        setToggle(false);
+        disptach(reduxClear());
+      } else {
+        setToggle(true);
+      }
+    } catch (error) {
+      setToggle(false);
+      disptach(reduxClear());
+    }
+  }, [disptach, markDownText, questionId, title, toggle]);
+
+  const clickCancelButton = useCallback(() => {
     setToggle((prev) => !prev);
-  }, []);
+    disptach(reduxClear());
+  }, [disptach]);
 
   return (
     <>
       <S.PostContent>
         {toggle ? (
-          <MarkDownRendering className="post-content" markDownText={body} />
-        ) : (
           <TabMenu>
             <TabList tabButtonList={["질문 수정", "미리 보기"]} />
             <TabPanel index={0}>
               <MarkDownToolBar />
-              <MarkDownInput />
+              <MarkDownInput className="markdown-input" initialText={renewBody} />
             </TabPanel>
             <TabPanel index={1} className="preview">
-              <MarkDownRendering markDownText={body} />
+              <MarkDownRendering markDownText={markDownText as string} />
             </TabPanel>
           </TabMenu>
+        ) : (
+          <MarkDownRendering className="post-content" markDownText={renewBody} />
         )}
         {author_id === token_id && (
           <div className="button-wrapper">
+            {toggle && (
+              <Button
+                onClick={clickCancelButton}
+                isLinked={false}
+                className="cancel-btn"
+                buttonColor="#266ce6"
+                fontColor="white"
+              >
+                취소하기
+              </Button>
+            )}
             <Button
               onClick={clickModifyButton}
               isLinked={false}
--- src/components/Question/PostContent/style.ts
+++ src/components/Question/PostContent/style.ts
@@ -2,20 +2,33 @@ import styled from "styled-components";
 
 export const PostContent = styled.section`
   padding: 24px 0;
+  margin-bottom: 32px;
   border-bottom: 1px solid #bababa;
   h1 {
     font-size: 1.2rem;
   }
-  .post-content {
-    min-height: 400px;
+  .markdown-input {
+    min-height: 180px;
+    background: white;
+    .CodeMirror-wrap {
+      height: 100%;
+    }
+  }
+  .post-content,
+  .tab-panel {
+    min-height: 200px;
   }
   .button-wrapper {
     display: flex;
     justify-content: flex-end;
     width: 100%;
   }
-  .modify-btn {
+  .modify-btn,
+  .cancel-btn {
     padding: 8px 16px;
     margin-top: 16px;
+  }
+  .cancel-btn {
+    margin-right: 8px;
   }
 `;
