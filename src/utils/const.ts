4343c7933fe707a95708023bc1b6a9dcfc2ffff3
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-27 22:51:19 +0900

    [추가] : const.ts 파일 추가

---
+++ src/utils/const.ts
@@ -0,0 +1,16 @@
+// 질문글, 프로젝트 작성 시 마크다운 툴바
+export const WRITE_PAGE_MARKDOWN_TOOLBAR = [
+  "h1",
+  "h2",
+  "h3",
+  "bold",
+  "italic",
+  "strike",
+  "quote",
+  "link",
+  "blockquote",
+  "image",
+];
+
+// 수정, 답변에서 마크다운 툴바
+export const BASIC_MARKDOWN_TOOLBAR = ["h1", "h2", "h3", "bold", "italic", "strike", "quote", "link", "blockquote"];
