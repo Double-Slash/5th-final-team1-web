ffa94a3ff00106495f391ed7835ac7991169f6da
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-28 01:15:50 +0900

    [변경] : 질문글 작성, 프로젝트 작성 페이지 Context API 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -11,8 +11,9 @@ const Question = loadable(() => import("@pages/Question"));
 const Project = loadable(() => import("@pages/Project"));
 const Projects = loadable(() => import("@pages/Projects"));
 const Search = loadable(() => import("@pages/Search"));
-const WriteQuestion = loadable(() => import("@pages/WriteQuestion"));
-const WriteProject = loadable(() => import("@pages/WriteProject"));
+// const User = loadable(() => import("@pages/User"));
+const WriteQuestion = loadable(() => import("@store/WriteQuestion"));
+const WriteProject = loadable(() => import("@store/WriteProject"));
 
 const Router = () => {
   return (
@@ -26,6 +27,7 @@ const Router = () => {
           <Route path="/project" component={Projects} exact />
           <Route path="/project/:id" component={Project} exact />
           <Route path="/search" component={Search} exact />
+          {/* <Route path="/me" component={User} exact /> */}
           <Route path="/write/question" component={WriteQuestion} exact />
           <Route path="/write/project" component={WriteProject} exact />
           <Route path="/callback" component={Callback} exact />
