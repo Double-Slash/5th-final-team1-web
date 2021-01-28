a947a210ac0b2ee50d04dd96bf2d77edee812f4e
chamincheol <54410332+chamincheol@users.noreply.github.com>
2021-01-29 01:19:32 +0900

    [변경] : button disabled 적용

--- src/common/Atoms/Button/index.tsx
+++ src/common/Atoms/Button/index.tsx
@@ -3,49 +3,53 @@ import { Link } from "react-router-dom";
 import * as S from "./style";
 
 /**
- * @param onClick 버튼을 클릭했을 때 실행되는 함수
- * @param isLinked 페이지 이동을 위한 버튼인지 체크
- * @param width 버튼 크기의 사이즈
  * @param buttonColor 버튼의 색상
- * @param fontColor 버튼 글자의 색상
- * @param type 버튼의 type
  * @param className 추가적인 버튼 classname
+ * @param disabled 버튼 비활성화 여부
+ * @param fontColor 버튼 글자의 색상
+ * @param isLinked 페이지 이동을 위한 버튼인지 체크
+ * @param onClick 버튼을 클릭했을 때 실행되는 함수
  * @param pathname 페이지 이동 버튼일 때 to property의 pathname
+ * @param type 버튼의 type
+ * @param width 버튼 크기의 사이즈
  */
 
 export interface ButtonProps {
-  onClick: () => void;
-  isLinked: boolean;
-  width?: string;
   buttonColor?: string;
-  fontColor?: string;
-  pathname?: string;
-  className?: string;
-  type?: "submit" | "button";
   children?: React.ReactNode;
+  className?: string;
+  disabled?: boolean;
+  fontColor?: string;
+  isLinked: boolean;
+  onClick?: () => void;
+  pathname?: string;
+  type?: "submit" | "button";
+  width?: string;
 }
 
 const Button = ({
-  onClick,
-  width,
   buttonColor,
+  children,
+  className,
+  disabled,
   fontColor,
   isLinked,
+  onClick,
   pathname,
-  className,
   type,
-  children,
+  width,
 }: ButtonProps) => {
   // 링크형 버튼일 경우
   if (isLinked) {
     return (
       <S.Button
+        buttonColor={buttonColor}
+        className={className}
+        disabled={disabled}
+        fontColor={fontColor}
         onClick={onClick}
         type={type}
         width={width}
-        buttonColor={buttonColor}
-        fontColor={fontColor}
-        className={className}
       >
         <Link to={`/${pathname}`}>{children}</Link>
       </S.Button>
@@ -56,12 +60,13 @@ const Button = ({
   return (
     <>
       <S.Button
+        buttonColor={buttonColor}
+        className={className}
+        disabled={disabled}
+        fontColor={fontColor}
         onClick={onClick}
         type={type}
         width={width}
-        buttonColor={buttonColor}
-        fontColor={fontColor}
-        className={className}
       >
         {children}
       </S.Button>
